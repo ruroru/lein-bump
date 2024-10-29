fd1a1ba8dab43a6aaca53b6ba66b41edabcf8f27
Jj <keistaslogairkosulys@gmail.com>
2024-10-29 20:23:24 +0100

    Allow auto-stepping of project version in cd pipeline

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.clojars.jj/bump "1.0.0"
+(defproject org.clojars.jj/bump "1.0.1-SNAPSHOT"
   :description "Lein plugin that helps updating version in project.clj"
   :url "https://github.com/ruroru/lein-bump"
   :license {:name "EPL-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/core.match "1.1.0"]]
 
+  :plugins [[org.clojars.jj/bump "1.0.0"]]
   :deploy-repositories [["clojars" {:url      "https://repo.clojars.org"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass}]]
