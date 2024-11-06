b153be924f7b1ab4de3b732d5db492f67b944233
Jj <keistaslogairkosulys@gmail.com>
2024-11-06 14:53:34 +0100

    cleanup tests

--- test/leiningen/bump_test.clj
+++ test/leiningen/bump_test.clj
@@ -6,19 +6,18 @@
     [mock-clj.core :as mock]))
 
 (defn- get-mock-project-clj [version] (str "(defproject lein-versions-plugin \"" version "\" :description \"F\" :url \"h\" :dependencies [[dependency-1 \"5.4.8\"] [dependency-2 \"5.4.5\"]])\n"))
+(defn- get-multi-project-mock-clj [version] (str "(defproject lein-versions-plugin \"" version "\" :description \"F\" :sub [\"sub-1\" \"sub2\"]:url \"h\" :dependencies [[dependency-1 \"5.4.8\"] [dependency-2 \"5.4.5\"]])\n"))
+
 
 (def dev "dev")
 
 (defn- verify-not-written []
   (is (= (mock/call-count spit) 0)))
 
-(defn- verify-spit [args call-count]
-  (is (= (mock/call-count spit) call-count))
-  (is (= (mock/calls spit) args)))
-
-(defn- verify-spit-call [version]
-  (let [expected-spit-arg [(list "project.clj" (get-mock-project-clj version))]]
-    (verify-spit expected-spit-arg 1)))
+(defn- verify-project-clj-write [version]
+  (let [args [(list "project.clj" (get-mock-project-clj version))]]
+    (is (= (mock/calls spit) args))
+    (is (= (mock/call-count spit) 1))))
 
 (deftest prepare-for-release
   (testing "Stepping patch version"
@@ -27,7 +26,7 @@
         [spit nil
          slurp (get-mock-project-clj (:version project))]
         (plugin/bump project "patch")
-        (verify-spit-call "5.4.9"))))
+        (verify-project-clj-write "5.4.9"))))
 
   (testing "Stepping patch snapshot version"
     (let [project {:version "5.4.8-SNAPSHOT"}]
@@ -35,7 +34,7 @@
         [spit nil
          slurp (get-mock-project-clj (:version project))]
         (plugin/bump project "patch")
-        (verify-spit-call "5.4.8"))))
+        (verify-project-clj-write "5.4.8"))))
 
   (testing "Stepping major version"
     (let [project {:version "5.4.8-SNAPSHOT"}]
@@ -43,7 +42,7 @@
         [spit nil
          slurp (get-mock-project-clj (:version project))]
         (plugin/bump project "major")
-        (verify-spit-call "6.0.0"))))
+        (verify-project-clj-write "6.0.0"))))
 
   (testing "Stepping minor version"
     (let [project {:version "5.4.8-SNAPSHOT"}]
@@ -51,7 +50,7 @@
         [spit nil
          slurp (get-mock-project-clj (:version project))]
         (plugin/bump project "minor")
-        (verify-spit-call "5.5.0")))))
+        (verify-project-clj-write "5.5.0")))))
 
 (deftest prepare-for-new-dev-iteration
   (testing "Prepare for new development iteration for 6.0.0"
@@ -60,7 +59,7 @@
         [spit nil
          slurp (get-mock-project-clj (:version project))]
         (plugin/bump project dev)
-        (verify-spit-call "6.0.1-SNAPSHOT"))))
+        (verify-project-clj-write "6.0.1-SNAPSHOT"))))
 
   (testing "Prepare for new development iteration for 5.4.9-SNAPSHOT"
     (let [project {:version "5.4.9-SNAPSHOT"}]
@@ -76,7 +75,7 @@
         [spit nil
          slurp (get-mock-project-clj (:version project))]
         (plugin/bump project dev)
-        (verify-spit-call "5.4.9-SNAPSHOT")))))
+        (verify-project-clj-write "5.4.9-SNAPSHOT")))))
 
 (deftest set-version
   (testing "set version to 6.0.0"
@@ -85,7 +84,7 @@
         [spit nil
          slurp (get-mock-project-clj (:version project))]
         (plugin/bump project "set" "1.0.0")
-        (verify-spit-call "1.0.0")))))
+        (verify-project-clj-write "1.0.0")))))
 
 (deftest get-version
   (testing "get version for 5.4.8"
