c03876876e7e0f2eddfa8741b61d7e8a6c92b36d
ruroru <111705692+ruroru@users.noreply.github.com>
2025-02-02 11:44:35 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/bump "1.0.4"
+(defproject org.clojars.jj/bump "1.0.5-SNAPSHOT"
   :description "Lein plugin that helps updating version in project.clj"
   :url "https://github.com/ruroru/lein-bump"
   :license {:name "EPL-2.0"
